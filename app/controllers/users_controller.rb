1208036fc34a30f7b1a1d4f6626342ce808fe039
jackismoe <jacksonmo@me.com>
2020-07-17 14:27:09 -0400

    Begin creation of User actions: New, Create, Index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+
+  def index
+
+  end
+
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to users_path(@user)
+    else
+      render :new
+    end
+    byebug
+  end
+
+  private
+
+  def user_params
+     params.require(:user).permit(:name, :email, :password_digest)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :reviews
   has_many :instruments # created
   has_many :brands, through: :instruments
   has_many :categories, through: :instruments
   has_one :cart
   has_many :reviewed_instruments, through: :reviews, source: :instrument # reviewed
+
+  validates_presence_of :name 
+  validates_presence_of :email
 end
