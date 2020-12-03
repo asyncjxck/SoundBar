c39976d5dd4d148bc08f9059b54a17d52e676d57
jackismoe <jacksonmo@me.com>
2020-12-03 16:33:59 -0500

    Remove sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_15_213907) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "brands", force: :cascade do |t|
     t.string "name"
     t.integer "category_id"
